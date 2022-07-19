4e3fd817297139e8a9c8af03f910e728eb10ed86
Arun Moorthy <arunmoorthy@Aruns-MacBook-Air-2.local>
2022-07-19 14:16:42 -0400

    time change

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -11,7 +11,7 @@ const Result = ({ setInterVisible, success }: ResultProps) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setInterVisible(false);
-    }, 1500);
+    }, 5);
     return () => clearTimeout(timer);
   }, []);
 
