e2af484cbd7bd05ddab05c5f1efff2cec0f15652
Arun Moorthy <arunmoorthy@Aruns-MacBook-Air-2.local>
2022-07-20 09:48:45 -0400

    change Ts and Ls

--- src/pages/Instructions.tsx
+++ src/pages/Instructions.tsx
@@ -1,6 +1,9 @@
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/Instructions.module.css";
+import blackT from "../assets/blackT.png";
+import blackL from "../assets/blackL.png";
+import { height } from "@mui/system";
 
 export default function Instructions() {
   const navigate = useNavigate();
@@ -10,18 +13,39 @@ export default function Instructions() {
       <h1>Instructions</h1>
       <div className={styles.writing}>
         <p>
-          Thank you for doing this experiment! There are 200 images. For each
-          trial, your goal is to click the target, which is the letter T. There
-          will also be a distractor, which is the letter L. Half of the images
-          will contain one target T. The other half of the images will be absent
-          trials, meaning that they will not have a target letter. If you find a
-          target, click the T to move to the image. However, if you believe that
-          there is no T, click the space bar to report an absent trial.
+          Thank you for doing this experiment! There are 200 images. In each
+          image, you will see some “L”s in different orientations. You are
+          looking for a target letter, “T”. There will be a T on about half of
+          the images. If the T is present, you should click on it. If it is
+          absent, you should press the space bar. Work as quickly and accurately
+          as you can.
         </p>
         <p>
           Perform the task for each image as quickly and accurately as possible.
         </p>
       </div>
+      <div>
+        <div className={styles.header}>
+          <h2>The Ts and Ls:</h2>
+        </div>
+        <div className={styles.preview}>
+          <div className={styles.L}>
+            <figure>
+              <img src={blackL} id="blkL" width = "200px" height= "200px" />
+              <figcaption>Ls are the distractors</figcaption>
+            </figure>
+          </div>
+          <div className={styles.T}>
+            <figure>
+            <img src={blackT} width = "200px" height= "200px" />
+              <figcaption>T is the target</figcaption>
+            </figure>
+            
+          </div>
+          
+        </div>
+      </div>
+
       <div className={styles.startExp}>
         <Button
           variant="contained"
